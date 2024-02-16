671d8befa1fc42ac56c4dcf70db091bdd7b68f81
al02810418 <157439151+al02810418@users.noreply.github.com>
2024-02-16 12:11:47 -0600

    Add files via upload

---
+++ actividad4iosapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  actividad4iosapp
+//
+//  Created by Adrian Rivera on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
